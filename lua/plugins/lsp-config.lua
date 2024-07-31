4782eb146415e5448f5c6860f7a4e1258b416da7
irmansyah <irmansyahproject@gmail.com>
2024-07-31 18:22:49 +0700

    add image support in terminal kitty but commented it;

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,8 +2,15 @@ return {
   {
     "williamboman/mason.nvim",
     lazy = false,
+    ensure_installed = {
+      "rust-analyzer"
+    },
+    -- dependencies = {
+    --   'williamboman/mason-lspconfig.nvim',
+    --   'WhoIsSethDaniel/mason-tool-installer.nvim',
+    -- },
     config = function()
-      require("mason").setup()
+      require('mason').setup {}
     end,
   },
   {
@@ -12,15 +19,12 @@ return {
     opts = {
       auto_install = true,
     },
-  },
-  {
-    "simrat39/rust-tools.nvim",
-    config = function()
-      require("rust-tools").setup({})
-    end,
+    ensure_installed = { "lua_ls", "rust_analyzer" },
+    automatic_installation = true,
   },
   {
     "neovim/nvim-lspconfig",
+    lazy = false,
     init_options = {
       configurationSection = { "html", "css", "javascript" },
       embeddedLanguages = {
@@ -33,24 +37,70 @@ return {
         rust = "html",
       },
     },
-    lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
+      local util = require('lspconfig/util')
 
-      lspconfig.rust_analyzer.setup({
+      lspconfig.dartls.setup {
         capabilities = capabilities
-      })
-      lspconfig.dartls.setup({
+      }
+      lspconfig.tsserver.setup {
         capabilities = capabilities
-      })
-      lspconfig.tsserver.setup({
+      }
+      lspconfig.lua_ls.setup {
         capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
+      }
+      lspconfig.html.setup({
         capabilities = capabilities
       })
 
+      lspconfig.cssls.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.rust_analyzer.setup {
+        capabilities = capabilities,
+        filetypes = { "rust" },
+        root_dir = util.root_pattern("Cargo.toml"),
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true
+            },
+            procMacro = {
+              enable = true,
+            },
+          }
+        },
+        on_attach = function(client, bufnr)
+          local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+          buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+          -- local opts = { noremap = true, silent = true }
+
+          -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+          -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+          -- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+          -- buf_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+          -- buf_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+          -- buf_set_keymap('n', '<space>wa', '<Cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+          -- buf_set_keymap('n', '<space>wr', '<Cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+          -- buf_set_keymap('n', '<space>wl', '<Cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+          -- buf_set_keymap('n', '<space>D', '<Cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+          -- buf_set_keymap('n', '<space>rn', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
+          -- buf_set_keymap('n', '<space>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+          -- buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
+          -- buf_set_keymap('n', '<space>e', '<Cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+          -- buf_set_keymap('n', '[d', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+          -- buf_set_keymap('n', ']d', '<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+          -- buf_set_keymap('n', '<space>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+          -- buf_set_keymap("n", "<space>f", "<Cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+        end,
+        flags = {
+          debounce_text_changes = 150,
+        }
+      }
       vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {})
       vim.keymap.set('n', '[e', vim.diagnostic.goto_prev, {})
       vim.keymap.set('n', ']e', vim.diagnostic.goto_next, {})
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,72 +1,144 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1001, -- this plugin needs to run before anything else
+    opts = {
+      rocks = { "magick" },
+    },
   },
-  config = function()
-    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-    vim.fn.sign_define("DiagnosticSignError",
-      { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn",
-      { text = " ", texthl = "DiagnosticSignWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo",
-      { text = " ", texthl = "DiagnosticSignInfo" })
-    vim.fn.sign_define("DiagnosticSignHint",
-      { text = "󰌵", texthl = "DiagnosticSignHint" })
+  -- {
+  --   "3rd/image.nvim",
+  --   -- branch = "release-please--branches--master",
+  --   event = "VeryLazy",
+  --   -- dependencies = {
+  --   --   {
+  --   --     "nvim-treesitter/nvim-treesitter",
+  --   --     build = ":TSUpdate",
+  --   --     config = function()
+  --   --       require("nvim-treesitter.configs").setup({
+  --   --         ensure_installed = { "markdown" },
+  --   --         highlight = { enable = true },
+  --   --       })
+  --   --     end,
+  --   --   },
+  --   -- },
+  --   opts = {
+  --     backend = "kitty",
+  --     rocks = {
+  --       hererocks = true
+  --     },
+  --     integrations = {
+  --       markdown = {
+  --         enabled = true,
+  --         clear_in_insert_mode = false,
+  --         download_remote_images = true,
+  --         only_render_image_at_cursor = false,
+  --         filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+  --       },
+  --       neorg = {
+  --         enabled = true,
+  --         clear_in_insert_mode = false,
+  --         download_remote_images = true,
+  --         only_render_image_at_cursor = false,
+  --         filetypes = { "norg" },
+  --       },
+  --     },
+  --     max_width = nil,
+  --     max_height = nil,
+  --     max_width_window_percentage = nil,
+  --     max_height_window_percentage = 50,
+  --     kitty_method = "normal",
+  --   },
+  -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim",
+    },
+    config = function()
+      -- If you want icons for diagnostic errors, you'll need to define them somewhere:
+      vim.fn.sign_define("DiagnosticSignError",
+        { text = " ", texthl = "DiagnosticSignError" })
+      vim.fn.sign_define("DiagnosticSignWarn",
+        { text = " ", texthl = "DiagnosticSignWarn" })
+      vim.fn.sign_define("DiagnosticSignInfo",
+        { text = " ", texthl = "DiagnosticSignInfo" })
+      vim.fn.sign_define("DiagnosticSignHint",
+        { text = "󰌵", texthl = "DiagnosticSignHint" })
 
-    require('neo-tree').setup({
-      default_component_configs = {
-        file_size = {
-          enabled = false,
+      require('neo-tree').setup({
+        default_component_configs = {
+          file_size = {
+            enabled = false,
+          },
+          type = {
+            enabled = false,
+          },
+          last_modified = {
+            enabled = false,
+          },
+          created = {
+            enabled = false,
+          },
         },
-        type = {
-          enabled = false,
+        diagnostics = {
+          enable = true,
+          icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+          },
         },
-        last_modified = {
-          enabled = false,
+        window = {
+          position = "bottom",
+          width = 32,
+          height = 20,
+          ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
         },
-        created = {
-          enabled = false,
+        filesystem = {
+          window = {
+            width = 32,
+            height = 20,
+            mappings = {
+              ["<leader>p"] = "image_kitty",
+            },
+          },
+          commands = {
+            image_kitty = function(state)
+              local node = state.tree:get_node()
+              if node.type == "file" then
+                require("image_preview").PreviewImage(node.path)
+              end
+            end,
+          }
         },
-      },
-      diagnostics = {
-        enable = true,
-        icons = {
-          hint = "",
-          info = "",
-          warning = "",
-          error = "",
-        },
-      },
-      window = {
-        position = "bottom",
-        width = 32,
-        height = 20,
-      },
-    })
+      })
 
-    local setkeymap = vim.api.nvim_set_keymap
+      local setkeymap = vim.api.nvim_set_keymap
 
-    function OpenNeoTree(neotree_command)
-      local bufnrs = vim.api.nvim_list_bufs()
-      for _, bufnr in ipairs(bufnrs) do
-        local bufname = vim.api.nvim_buf_get_name(bufnr)
-        if bufname == "" or bufname == "[No Name]" then
-          vim.api.nvim_buf_delete(bufnr, { force = true })
+      function OpenNeoTree(neotree_command)
+        local bufnrs = vim.api.nvim_list_bufs()
+        for _, bufnr in ipairs(bufnrs) do
+          local bufname = vim.api.nvim_buf_get_name(bufnr)
+          if bufname == "" or bufname == "[No Name]" then
+            vim.api.nvim_buf_delete(bufnr, { force = true })
+          end
         end
+        vim.cmd(neotree_command)
       end
-      vim.cmd(neotree_command)
-    end
 
-    -- vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal toggle<CR>", {})
-    -- vim.keymap.set("n", "<C-f>", ":Neotree filesystem toggle<CR>", { noremap = true, silent = true })
-    -- setkeymap('n', '<C-t>', 'lua open_neotree_or_close_no_name(":Neotree filesystem reveal toggle")<CR>', {})
-    setkeymap('n', '<C-t>', ':lua OpenNeoTree(":Neotree filesystem reveal toggle")<CR>',
-      { noremap = true, silent = true })
-    setkeymap("n", "<C-f>", ':lua OpenNeoTree(":Neotree filesystem toggle")<CR>', {})
-  end,
+      -- vim.keymap.set("n", "<C-t>", ":Neotree filesystem reveal toggle<CR>", {})
+      -- vim.keymap.set("n", "<C-f>", ":Neotree filesystem toggle<CR>", { noremap = true, silent = true })
+      -- setkeymap('n', '<C-t>', 'lua open_neotree_or_close_no_name(":Neotree filesystem reveal toggle")<CR>', {})
+      setkeymap('n', '<C-t>', ':lua OpenNeoTree(":Neotree filesystem reveal toggle")<CR>',
+        { noremap = true, silent = true })
+      setkeymap("n", "<C-f>", ':lua OpenNeoTree(":Neotree filesystem toggle")<CR>', { noremap = true, silent = true })
+      setkeymap("n", "<C-g>", ':Neotree float git_status<CR>', {})
+    end,
+  },
 }
